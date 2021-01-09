81e09a04176866788a91c9c460e1d7f02d2a64b6
Draemon <bryce.harrington15@gmail.com>
2021-01-08 22:38:46 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('All your base are belong us'));
+app.get('/', (req, res) => res.send('Testing. Testing. 1, 2, 3'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
